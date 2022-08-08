192aabc3d72bdbe7349004f3e4e9373473b863b4
Sameer-Waseem <sameerwaseem969@gmail.com>
2022-08-08 22:37:31 +0500

    change

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const genres = require("./routes/genres");
 const customers = require("./routes/customers");
 
 const app = express();
+require("./startup/prod")(app);
 
 app.use(express.json());
 app.use(express.static("public"));
@@ -12,14 +13,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/api/genre", genres);
 app.use("/api/customer", customers);
 
-require("./startup/prod")(app);
-
 const port = process.env.PORT || 5000;
 
 mongoose
   .connect(
-    "mongodb+srv://sameerwaseem:sameer123@vidly.fbjbeoz.mongodb.net/?retryWrites=true&w=majority",
-    { useNewUrlParser: true, useUnifiedTopology: true }
+    "mongodb+srv://sameerwaseem:sameer123@vidly.fbjbeoz.mongodb.net/?retryWrites=true&w=majority"
   )
   .then(() => console.log("Connected to MongoDB..."))
   .catch((error) => console.log("Could not connected to MongoDB...", error));
